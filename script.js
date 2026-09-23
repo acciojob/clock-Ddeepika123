dcbb9ef31b532260f21e0c825d57b26494439892
Dpka Singh <112195696+Ddeepika123@users.noreply.github.com>
2026-09-24 00:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateTimer() {
+    let currentTime = new Date();
+
+    document.getElementById("timer").innerText = currentTime;
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
